d4caf01cacb47d4b7e452151ba277ea78d8d9411
matheus-bernat <matheus.bernat@gmail.com>
2022-08-16 17:22:28 -0300

    First version of YouthGroupCard

---
+++ lib/models/youth_group.dart
@@ -0,0 +1,15 @@
+﻿import 'package:flutter/material.dart';
+
+class YouthGroup {
+  final int id;
+  final String name;
+  final String city;
+  bool isVisible;
+
+  YouthGroup({
+    required this.id,
+    required this.name,
+    required this.city,
+    required this.isVisible,
+  });
+}
---
+++ lib/youth_group_card.dart
@@ -0,0 +1,69 @@
+﻿import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:wefind/models/youth_group.dart';
+
+class YouthGroupCard extends StatelessWidget {
+  final YouthGroup youthGroup;
+
+  YouthGroupCard(this.youthGroup);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.local_fire_department),
+            title: Text(youthGroup.name,
+                style: Theme.of(context).textTheme.headline6),
+          ),
+          Image.asset('assets/images/adoray2.jpg'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Chip(
+                avatar: const Icon(
+                  Icons.access_time_sharp,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  'Mercredi 19h30',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                backgroundColor: Theme.of(context).backgroundColor,
+              ),
+              Chip(
+                avatar: const Icon(
+                  Icons.local_fire_department_outlined,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  'Louange',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                backgroundColor: Theme.of(context).backgroundColor,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {
+                  print('insta pressed');
+                },
+                icon: const FaIcon(FontAwesomeIcons.instagram),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(FontAwesomeIcons.facebook),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
