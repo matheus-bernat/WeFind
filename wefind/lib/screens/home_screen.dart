e1478d8095d769a0ab9e2f80312037cfb09884d1
matheus-bernat <matheus.bernat@gmail.com>
2022-10-16 09:58:20 +0200

    fetching of cities working

--- wefind/lib/screens/home_screen.dart
+++ wefind/lib/screens/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:wefind/screens/group_form_screen.dart';
 
 import '../widgets/youth_group_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../dummy_data.dart';
+// import '../dummy_data.dart';
 import '../models/youth_group.dart';
 import '../models/city.dart';
 
@@ -13,23 +13,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
-  final Stream<QuerySnapshot> youthGroups = FirebaseFirestore.instance
-      .collection('/cities/lausanne/youth-groups')
-      .snapshots();
+  late Future<List<City>> citiesFuture;
 
-  // Create a reference to the cities collection
-  final citiesRef = FirebaseFirestore.instance.collection('cities');
+  @override
+  void initState() {
+    super.initState();
+    citiesFuture = getCities();
+  }
 
-  List<City> cities = [];
-
-  Future<void> getData() async {
-    // Get docs from collection reference
+  Future<List<City>> getCities() async {
+    final citiesRef = FirebaseFirestore.instance.collection('cities');
     QuerySnapshot querySnapshot = await citiesRef.get();
+    List<City> cities = [];
     for (var doc in querySnapshot.docs) {
       City newCity = City(name: doc.get('name'));
       cities.add(newCity);
     }
+    return cities;
   }
 
   int? _value = 0;
@@ -78,55 +78,69 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 25),
-              child: Wrap(
-                children: List<Widget>.generate(cities.length, (index) {
-                  return ChoiceChip(
-                    label: Text(
-                      cities[index].name, // here I need the "name" field of the city document
-                      style: theme.textTheme.bodyText1,
-                    ),
-                    selected: _value == index,
-                    onSelected: (bool selected) {
-                      setState(() {
-                        _value = selected ? index : null;
-                        // print(_chosenCity.name);
-                      });
-                    },
-                  );
-                }),
-              ),
-            ),
-            Expanded(
-              child: SizedBox(
-                width: 700,
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: youthGroups,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
+              child: FutureBuilder(
+                  future: citiesFuture,
+                  builder: (context, snapshot) {
                     if (snapshot.hasError) {
-                      Text('Something went wrong.');
+                      final error = snapshot.error;
+                      return Text('$error');
+                    } else if (snapshot.hasData) {
+
+                      List<City>? data = snapshot.data! as List<City>?;
+
+                      return Wrap(
+                        children: List<Widget>.generate(data!.length, (index) {
+                          return ChoiceChip(
+                            label: Text(
+                              data[index].name,
+                              style: theme.textTheme.bodyText1,
+                            ),
+                            selected: _value == index,
+                            onSelected: (bool selected) {
+                              setState(() {
+                                _value = selected ? index : null;
+                                // print(_chosenCity.name);
+                              });
+                            },
+                          );
+                        }),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
                     }
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Text('Loading...');
-                    }
-                    final data = snapshot.requireData;
-                    return ListView.builder(
-                      itemCount: data.size,
-                      itemBuilder: (context, index) {
-                        // Create a YouthGroup object
-                        YouthGroup yg = YouthGroup(
-                          name: data.docs[index]['name'],
-                          church: data.docs[index]['church'],
-                          who: data.docs[index]['who'],
-                          when: data.docs[index]['when'],
-                        );
-                        return YouthGroupCard(yg);
-                      },
-                    );
-                  },
-                ),
-              ),
+                  }),
             ),
+            // Expanded(
+            //   child: SizedBox(
+            //     width: 700,
+            //     child: StreamBuilder<QuerySnapshot>(
+            //       stream: youthGroups,
+            //       builder: (BuildContext context,
+            //           AsyncSnapshot<QuerySnapshot> snapshot) {
+            //         if (snapshot.hasError) {
+            //           Text('Something went wrong.');
+            //         }
+            //         if (snapshot.connectionState == ConnectionState.waiting) {
+            //           return Text('Loading...');
+            //         }
+            //         final data = snapshot.requireData;
+            //         return ListView.builder(
+            //           itemCount: data.size,
+            //           itemBuilder: (context, index) {
+            //             // Create a YouthGroup object
+            //             YouthGroup yg = YouthGroup(
+            //               name: data.docs[index]['name'],
+            //               church: data.docs[index]['church'],
+            //               who: data.docs[index]['who'],
+            //               when: data.docs[index]['when'],
+            //             );
+            //             return YouthGroupCard(yg);
+            //           },
+            //         );
+            //       },
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
